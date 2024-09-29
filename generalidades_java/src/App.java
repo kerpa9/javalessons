a210937476201a79045d83166f4fb30738fe2774
Kevin Reyes <kerpa9@gmail.com>
2024-09-29 13:08:23 -0500

    Types dates and casting

--- generalidades_java/src/App.java
+++ generalidades_java/src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Use the SOPL
+        System.out.println("Bienvenido(a) a screen match!");
+        System.out.println("Película: Matrix");
+        // Types of datas primitives, reference and compose
+        // Text block
+        String lorem = """
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet blandit orci eu ultricies. Vestibulum porttitor mi nisi, et elementum ipsum euismod id. Fusce ut lectus id nibh posuere feugiat at eu mauris. Donec ut facilisis ante, ac tempus odio. Praesent non magna sit amet ex dignissim varius in eget sem.
+                        """;
+        int fechaDeLazamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDePelicula = 8.2;
+        String[] date = { "cama", "jota" };
+        char letra = 'o';
+        double media = (8.2 + 6.0 + 9.0) / 3;
+
+        int clasificacion = (int) (media / 2);
+
+        System.out.println(lorem + "fecha de lanzamiento: " + fechaDeLazamiento);
+        System.out.println(clasificacion);
+
     }
 }
